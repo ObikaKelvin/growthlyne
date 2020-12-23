69ed9c718501d805e5f6dabb704a14126ff9074b
ObikaKelvin <obikakelvin.2255@gmail.com>
2020-12-24 00:57:01 +0100

    fixed serviceWorker link

--- js/main.js
+++ js/main.js
@@ -161,7 +161,7 @@ window.addEventListener('resize', function () {
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
         navigator.serviceWorker
-        .register("/serviceWorker.js")
+        .register("../serviceWorker.js")
         .then(res => console.log("service worker registered"))
         .catch(err => console.log("service worker not registered", err))
     })
