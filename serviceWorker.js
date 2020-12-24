25a74e858afe4d8afb6faf8893e819557447a641
ObikaKelvin <obikakelvin.2255@gmail.com>
2020-12-24 12:31:25 +0100

    more service worker stuff

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,13 +1,13 @@
 const growthLyne = "growth-lyne-v1"
 const assets = [
   
-  "/index.html",
-  "/about.html",
-  "/contact.html",
-  "/knowledgelibrary.html",
-  "/offerings.html",
-  "/css/style.css",
-  "/js/main.js",
+  "/growthlyne/index.html",
+  "/growthlyne/about.html",
+  "/growthlyne/contact.html",
+  "/growthlyne/knowledgelibrary.html",
+  "/growthlyne/offerings.html",
+  "/growthlyne/css/style.css",
+  "/growthlyne/js/main.js",
   "/growthlyne/images/business-improvements-1024x576.jpg",
   
 ]
