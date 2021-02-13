7dec2a64d45fefaf1b23867af7eb5f8746529f03
ObikaKelvin <obikakelvin.2255@gmail.com>
2021-02-13 02:52:56 +0100

    added services update 2

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,11 +1,17 @@
-const growthLyne = "growth-lyne-v1"
+const GROWTHLYNE = "growth-lyne-v2";
+
+var CURRENT_CACHES = {
+  prefetch: 'prefetch-cache-v' + GROWTHLYNE
+};
+
+
 const assets = [
   
   "/growthlyne/index.html",
-  "/growthlyne//about.html",
-  "/growthlyne//contact.html",
-  "/growthlyne//knowledgelibrary.html",
-  "/growthlyne//offerings.html",
+  "/growthlyne/about.html",
+  "/growthlyne/contact.html",
+  "/growthlyne/knowledgelibrary.html",
+  "/growthlyne/offerings.html",
   "/growthlyne/css/style1.css",
   "/growthlyne/js/main.js",
   "/growthlyne/images/business-improvements-1024x576.jpg",
@@ -15,17 +21,17 @@ const assets = [
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(growthLyne).then(cache => {
+    caches.open(GROWTHLYNE).then(cache => {
       return cache.addAll(assets)
     })
   )
 });
 
 self.addEventListener("fetch", fetchEvent => {
-    fetchEvent.respondWith(
-      caches.match(fetchEvent.request).then(res => {
-        return res || fetch(fetchEvent.request)
-      })
-    )
+    // fetchEvent.respondWith(
+    //   caches.match(fetchEvent.request).then(res => {
+    //     return res || fetch(fetchEvent.request)
+    //   })
+    // )
 });
 
